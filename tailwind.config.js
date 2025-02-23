d2ecd5d1ecbe3c4b6cdcd0ff450feb5a1c3830e5
FarrelAdelio <farreladelioasm@gmail.com>
2025-02-23 21:50:09 +0700

    1

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary1: "#ffe374",
+        primary2: "#151f28",
+      },
+      fontFamily: {
+        poppins: "Poppins"
+      },
+    },
+  },
+  plugins: [],
+}
